10a6a153ea4af9188c4eb046f27794b9e9c41e07
rprakashdass <rprakashdass@gmail.com>
2024-03-29 16:16:23 +0530

    update index

--- App/static/script.js
+++ App/static/script.js
@@ -1,3 +1,5 @@
+let nofdata = 0
+let data = []
 
 function upload_sheet(){
     url = document.location.href
@@ -5,6 +7,25 @@ function upload_sheet(){
     alert(url)
 }
 
-function display(value) {
-    document.getElementById('display_input').innerHTML = value
+function display_len(value) {
+    nofdata = value
+    document.getElementById('display_len').innerHTML = value
+}
+
+function display_data(data){
+    input_data += data
+    document.getElementById('display_data').innerHTML = data;
+}
+
+function displayPast(nofdata, input_data){
+    document.getElementById('past-nofdata').innerHTML = nofdata;
+    document.getElementById('past-data').innerHTML = input_data;
+}
+
+function getInputs(data){
+    const nofdata =  data[0].value;
+    const input_data =  data[1].value;
+    data += input_data;
+    displayPast(nofdata,input_data);
+    // data.submit();
 }
--- App/upload_data2excel.py
+++ App/upload_data2excel.py
@@ -1,9 +1,18 @@
 from flask import (
-    Flask,Blueprint,render_template
+    Flask,Blueprint,render_template,request
 )
+
+from App import upload
 
 bp = Blueprint('upload_data2excel', __name__)
 
-@bp.route('/upload_data2excel/')
+@bp.route('/upload_data2excel/', methods=['GET','POST'])
 def upload_data2excelt():
     return render_template('upload_data2excelt.html')
+
+# @bp.route('/sendData')
+# def sendData():
+#     if request.method == 'POST':
+#         nofdata = request.nofdata
+#         data = request.data
+#     return 
